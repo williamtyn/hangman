56da279f9c77f11a97ed0fbc1240b2e4ac65bbd2
williamtyn <william@tyner.se>
2022-10-05 08:48:01 +0000

    add wordlist and function to generate random word and loop trough it

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+words = ['problem', 'green', 'moving', 'history', 'camera', 'glasses', 'shower', 'jacket', 'popular', 'pumpkin']
+
+def random_word(random):
+
+    """
+    Generates random word from list and loops trough the random word.
+    """
+    random = random.choice(words)
+    for letter in random:
+        print(letter)
+
+
+
+
+def main():
+    """
+    Runs all program functions
+    """
+    random_word(random)
+
+print('Welcome to my Hangman Game!')
+main()
