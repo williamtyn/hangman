46060be237165d42ea46ed1efcd6d035d3885579
williamtyn <william@tyner.se>
2022-10-06 09:30:37 +0000

    start of function to check if guess is in word, added guessed letters and lives

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import random
 
 words = ['egg', 'thanks', 'problem', 'green', 'moving', 'history', 'camera', 'glasses', 'shower', 'jacket', 'popular', 'pumpkin']
 
-def random_word(words):
+def random_word():
     """
     Generates random word from list words.
     """
@@ -10,23 +10,40 @@ def random_word(words):
     return word_random
 
 def letter_in_word(word):
-    guess = input('Please input a letter/word between A - Z\n').upper()
-    if (guess.isalpha()) == True:
-        #lives -= 1
-        #guessed_letters += guess
-        return guess
-    elif (guess.isalpha()) == True and guess == word:
-        Print('Congrats! You guessed the right word')
-    elif (guess.isalpha()) != True:
-        print('Your guess can only contain letters A - Z, try again!\n')
+    """
+    User input of letter/word and validating that input 
+    contains letter and nothing else
+    """
+    lives = 6
+    guessed_letters = []
+    while lives > 0:
+        print(word)
+        print(f'You have {lives} lives left and have guessed {guessed_letters}\n')
+        guess = input('Please input a letter/word between A - Z\n').upper()
+        if (guess.isalpha()) == True:
+            if guess in word:
+                guessed_letters += guess
+                print(guessed_letters)
+            elif guess != word:
+                lives -= 1
+                guessed_letters += guess
+                print(f'{lives} lives left')
+                print(f'You have guessed {guessed_letters}')    
+        elif (guess.isalpha()) != True:
+            print('Your guess can only contain letters A - Z, try again!\n')
+            break
+    print('You lost the Game!')
+            
     
-
-def main():
+def play_game():
     """
     Running main program
     """
-    word = random_word(words)
+    #lives = 6
+    #guessed_letters = []
+    word = random_word()
     print('Welcome to Hangman Game!\n')
     letter_in_word(word)
 
-main()
+
+play_game()
