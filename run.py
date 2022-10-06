70c41cc5592f285a0a2c33421d41a827537a8cf8
williamtyn <william@tyner.se>
2022-10-06 19:39:28 +0000

    add function for player to see rules before starting the game

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def letter_in_word(word):
     while lives > 0:
         print(word)
         print(hidden_word)
-        #print(f'You have {lives} lives left and have guessed {guessed_letters}\n')
+        print(f'You have {lives} lives left and have guessed {guessed_letters}\n')
         guess = input('Please input a letter/word between A - Z\n').upper()
         if (guess.isalpha()) == True:
             if guess in word and guess == word:
@@ -36,34 +36,44 @@ def letter_in_word(word):
                 hidden_word = ''.join(show_letters)
                 if '_' not in hidden_word:
                     guessed = True 
-                print(f'{guess} is in the word')
+                print(f'{guess} is in the word\n')
             elif guess != word:
                 lives -= 1
                 guessed_letters += guess
-                print(f'{guess} is NOT in the word')    
+                print(f'{guess} is NOT in the word\n')    
         elif (guess.isalpha()) == False:
             print('Your guess can only contain letters A - Z, try again!\n')
-            break #will be removed before deployment
     print('End of game\n')
 
+
 def play_again():
+    """
+    Ask player if they want to play again. If not the program ends.
+    """
     play = input('Do you want to play again? Please press Y\n').upper()
     if play == 'Y':
         play_game()
-    else break
+    else:
+        print('Thanks for playing Hangman!')
+
+def see_rules():
+    rules = input('Do you want to see the rules? Y/N\n').upper()
+    if rules == 'Y':
+        print('Hangman is game of words where you guess the secret word the computer shuffles.\nYou can press any letter between A – Z to guess if that letter is in the word.\nIf you think you know the word you can guess by typing it in.\nFor each wrong word or letter you will lose one of your lives.\nYou have 6 lives to guess the right word before it is Game Over!\n')
+    else:
+        play_game()
 
 def play_game():
     """
     Running main program
     """
     word = random_word()
-    print('Welcome to Hangman Game!\n')
     letter_in_word(word)
     play_again()
 
-
+print('Welcome to Hangman Game!\n')
+see_rules()
 play_game()
-
 
 """
 def game_over():
@@ -79,4 +89,20 @@ def game_over():
 *    *  *  *   ***   ****
 *    *  * *    *     * *
  ****    *     ***** *  *
- ')"""
+ ')
+print(game_over())
+"""
+"""
+def win_game
+*     *  ****  *     *
+ *   *  *    * *     *
+   *    *    * *     *
+   *    *    * *     *
+   *     ****   *****
+
+*       *  ***  *    *  *
+*       *   *   * *  *  *
+*   *   *   *   *  * *  *
+ * * * *    *   *   **  
+  *   *    ***  *    *  *
+"""
