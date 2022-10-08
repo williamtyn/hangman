994f75566c2216ef7c5f340587c83745dc4252c1
williamtyn <william@tyner.se>
2022-10-08 19:18:10 +0000

    fix PEP8 error code in run.py

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
-import random
+import random  # import random library to random words from words.py
 from words import words
 
 hangman = ["""
-   +---+
-   O   |
-  /|\  |
-  / \  |
-      ===
-    """]
+           +---+
+           O   |
+          /|\  |
+          / \  |
+              ===
+          """]
+
 
 def random_word():
     """
@@ -16,13 +17,16 @@ def random_word():
     word_random = random.choice(words).upper()
     return word_random
 
+
 def letter_in_word(word):
     """
     User input of letter/word and validating that input 
     contains letter and nothing else.
+
+    Code enumerate source https://www.youtube.com/watch?v=m4nEnsavl6w&t=423s
     """
-    lives = 3  #how many attempts the player have
-    guessed_letters = []  #will display already guessed letters for player
+    lives = 3  # how many attempts the player have
+    guessed_letters = []  # will display already guessed letters for player
     guessed = False
     hidden_word = '_' * len(word)
     while lives > 0:
@@ -37,13 +41,13 @@ def letter_in_word(word):
                 break
             elif guess in word and guess not in guessed_letters:
                 guessed_letters += guess
-                show_letters = list(hidden_word)  #code to enumerate is of inspiration from https://www.youtube.com/watch?v=m4nEnsavl6w&t=423s
-                indices = [i for i,letter in enumerate(word) if letter == guess]
-                for index in indices:
-                    show_letters[index] = guess
+                show_letters = list(hidden_word)  
+                ind = [i for i, letter in enumerate(word) if letter == guess]
+                for index in ind:  # code to enumerate is of inspiration
+                    show_letters[index] = guess  # from see above
                 hidden_word = ''.join(show_letters)
                 if '_' not in hidden_word:
-                    guessed is True 
+                    guessed is True
                 print(f'{guess} is in the word\n')
             elif guess in guessed_letters:
                 print(f'You have already guessed {guess}')
@@ -68,16 +72,21 @@ def play_again():
     else:
         print('Thanks for playing Hangman!')
 
+
 def see_rules():
     rules = input('Do you want to see the rules? Y/N\n').upper()
     if rules == 'Y':
-        print('Hangman is game of words where you guess the secret word the computer shuffles.')
-        print('You can press any letter between A – Z to guess if that letter is in the word.')
+        print('Hangman is game of words where you guess the secret word'
+              'the computer shuffles.')
+        print('You can press any letter between A – Z to guess if that letter'
+              'is in the word.')
         print('If you think you know the word you can guess by typing it in.')
         print('For each wrong word or letter you will lose one of your lives.')
-        print('You have 6 lives to guess the right word before it is Game Over!\n')
+        print('You have 6 lives to guess the right word before it is'
+              'Game Over!\n')
     else:
         play_game()
+
 
 def play_game():
     """
@@ -85,12 +94,16 @@ def play_game():
     """
     word = random_word()
     letter_in_word(word)
-    
+
 
 def choose_nickname():
+    """
+    As a player you can choose your player nickname
+    """
     nickname = input('Choose a nickname:\n').upper()
     print(f'Welcome {nickname}!\n')
     return nickname
+
 
 print('Welcome to Hangman Game!\n')
 print(hangman)
